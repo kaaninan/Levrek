cfb1e91006f9f255a36775f04f86629317c31b64
omerraker <omerraker@yahoo.com>
2016-11-01 23:25:59 +0300

    Documentation added

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # Levrek
 
 ## Levrek, Simple GitHub-CLI
+[![npm version](https://badge.fury.io/js/levrek.svg)](https://badge.fury.io/js/levrek)
 
-Kullanıcıların repolarını görmek için hazırlanmış projedir.
+Minimalistic GitHub Command Line Interface
 
+
+### Installation
+
+
+```sh
+$ npm install -g levrek
+```
+
+### Usage
+
+Easy-to use
+
+```sh
+$ levrek -u <username>
+```
+
+Like: 
+```sh
+$ levrek -u omerraker
+```
 
 ### TODO
-  - Dökümantasyon Hazırlanacak
-  - Baya Update Lazım
+  - Documentation
+  - More Updates
--- src/levrek.js
+++ src/levrek.js
@@ -1,42 +1,47 @@
-'use strict';
+/*jshint esversion: 6 */
+(function() {
+  "use strict";
 
-let colors = require('chalk');
-var program = require('commander');
-var request = require('request');
+  let colors = require('chalk');
+  let program = require('commander');
+  let request = require('request');
 
-program
-  .version(require('../package.json').version)
-  .option('-u, --user <username>','username (user)')
-  .parse(process.argv);
+  program
+    .version(require('../package.json').version)
+    .option('-u, --user <username>','username (user)')
+    .parse(process.argv);
 
-if (!program.user) {
-  console.error(colors.red('Enter a username!'));
-  return;
-}
-
-let options = {
-  headers: {
-    'User-Agent': 'omerraker'
-  }
-}
-
-options.url = 'https://api.github.com/users/' + program.user + '/repos';
-request.header = {'User-Agent':'omerraker'}
-
-request(options,function (error, response, body) {
-
-  if (error || response.statusCode !== 200) {
-    console.error(colors.red('Failed, try again!'));
+  if (!program.user) {
+    console.error(colors.red('Enter a username!'));
     return;
   }
 
-  let parsedData = JSON.parse(body);
-  console.log('Repository count: '+ parsedData.length);
-  for (let a of parsedData) {
-    console.log('');
-    console.log(colors.red('Repository Name: ') + colors.blue(a.name));
-    console.log(colors.red('Stargazers Count: ') + colors.blue(a.stargazers_count.toString()));
-    console.log('');
-    console.log(colors.white('__________________'));
-  }
-});
+  let options = {
+    headers: {
+      'User-Agent': 'omerraker'
+    }
+  };
+
+  options.url = 'https://api.github.com/users/' + program.user + '/repos';
+  request.header = {'User-Agent':'omerraker'};
+
+  request(options,function (error, response, body) {
+
+    if (error || response.statusCode !== 200) {
+      console.error(colors.red('Failed, try again!'));
+      return;
+    }
+
+    let parsedData = JSON.parse(body);
+    console.log('Repository count: '+ parsedData.length);
+    for (let a of parsedData) {
+      console.log('');
+      console.log(colors.red('Repository Name: ') + colors.blue(a.name));
+      console.log(colors.red('Stargazers Count: ') + colors.blue(a.stargazers_count.toString()));
+      console.log('');
+      console.log(colors.white('__________________'));
+    }
+  });
+
+
+})();
